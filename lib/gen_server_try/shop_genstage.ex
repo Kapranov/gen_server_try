a4a7145d5fc61ef9158058331bd47f12c2bdac91
Oleg G.Kapranov <lugatex@yahoo.com>
2018-12-04 19:56:55 +0200

    added docs to ShopGenstage

--- lib/gen_server_try/shop_genstage.ex
+++ lib/gen_server_try/shop_genstage.ex
@@ -23,31 +23,91 @@ defmodule GenServerTry.ShopGenstage do
   """
   def show(pid), do: GenStage.call(pid, :show)
 
-  @doc false
+  @doc """
+  Count all items in cart
+
+  ## Example
+
+      iex> {:ok, pid} = GenServerTry.ShopGenstage.start_link
+      iex> GenServerTry.ShopGenstage.count(pid)
+      0
+  """
   def count(pid), do: GenStage.call(pid, :count)
 
-  @doc false
+  @doc """
+  Delete first item from list in cart
+
+  ## Example
+
+      iex> {:ok, pid} = GenServerTry.ShopGenstage.start_link
+      iex> GenServerTry.ShopGenstage.fetch(pid)
+      :ok
+  """
   def fetch(pid), do: GenStage.call(pid, :fetch)
 
-  @doc false
+  @doc """
+  Reset all items and create empty list
+
+  ## Example
+
+      iex> {:ok, pid} = GenServerTry.ShopGenstage.start_link
+      iex> GenServerTry.ShopGenstage.reset(pid)
+      []
+  """
   def reset(pid), do: GenServer.call(pid, :reset)
 
-  @doc false
+  @doc """
+  Add item to cart
+
+  ## Example
+
+      iex> {:ok, pid} = GenServerTry.ShopGenstage.start_link
+      iex> GenServerTry.ShopGenstage.add(pid, "item-1")
+      :ok
+  """
   def add(pid, item), do: GenStage.cast(pid, {:add, item})
 
-  @doc false
+  @doc """
+  Update item to new item
+
+  ## Example
+
+      iex> {:ok, pid} = GenServerTry.ShopGenstage.start_link
+      iex> GenServerTry.ShopGenstage.add(pid, "item-1")
+      :ok
+      iex> GenServerTry.ShopGenstage.update(pid, "item-1", "item-0")
+      :ok
+  """
   def update(pid, old_item, new_item) do
     GenStage.cast(pid, {:update, old_item, new_item})
   end
 
-  @doc false
+  @doc """
+  Delete item from cart
+
+  ## Example
+
+      iex> {:ok, pid} = GenServerTry.ShopGenstage.start_link
+      iex> GenServerTry.ShopGenstage.del(pid, "item-1")
+      :ok
+  """
   def del(pid, item), do: GenStage.cast(pid, {:del, item})
 
-  @doc false
+  @doc """
+  Stop Shopping Cart GenServer
+
+  ## Example
+
+      iex> {:ok, pid} = GenServerTry.ShopGenstage.start_link
+      iex> GenServerTry.ShopGenstage.stop(pid)
+      :ok
+  """
   def stop(pid), do: GenStage.stop(pid, :normal, :infinity)
 
   @impl true
-  @doc false
+  @doc """
+  Invoked when the server is about to exit. It should do any cleanup required.
+  """
   def terminate(_reason, items) do
     IO.puts("We are all done shopping.")
     # credo:disable-for-next-line
@@ -60,20 +120,26 @@ defmodule GenServerTry.ShopGenstage do
   def init(state), do: {:producer, state}
 
   @impl true
-  @doc false
+  @doc """
+  Invoked to handle synchronous callback `call/3` messages: `:show`
+  """
   def handle_call(:show, _from, list) do
     {:reply, list, [], list}
   end
 
   @impl true
-  @doc false
+  @doc """
+  Invoked to handle synchronous callback `call/3` messages: `:count`
+  """
   def handle_call(:count, _from, list) do
     counter = Enum.count(list)
     {:reply, counter, [], list}
   end
 
   @impl true
-  @doc false
+  @doc """
+  Invoked to handle synchronous callback `call/3` messages: `:fetch`
+  """
   def handle_call(:fetch, _from, list) do
     item = list |> List.first
     updated = Enum.reject(list, &(&1 == item))
@@ -81,20 +147,27 @@ defmodule GenServerTry.ShopGenstage do
   end
 
   @impl true
-  @doc false
+  @doc """
+  Invoked to handle synchronous callback `call/3` messages: `:reset`
+  """
   def handle_call(:reset, _from, _list) do
     list = []
     {:reply, list, [], list}
   end
 
   @impl true
-  @doc false
+  @doc """
+  Invoked to handle asynchronous callback `cast/2` messages: `:add`
+  """
   def handle_cast({:add, item}, list) do
     updated = [item|list] |> List.flatten
     {:noreply, [], updated}
   end
 
-  @doc false
+  @impl true
+  @doc """
+  Invoked to handle asynchronous callback `cast/2` messages: `:update`
+  """
   def handle_cast({:update, old_item, new_item}, list) do
     [hd|_] =
       list
@@ -107,7 +180,10 @@ defmodule GenServerTry.ShopGenstage do
     {:noreply, [], updated}
   end
 
-  @doc false
+  @impl true
+  @doc """
+  Invoked to handle asynchronous callback `cast/2` messages: `:del`
+  """
   def handle_cast({:del, item}, list) do
     updated = Enum.reject(list, &(&1 == item))
     {:noreply, [], updated}
